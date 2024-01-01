8dd14a6455f09663ff5e03d9e12f077fcc84ac5d
EOussama <oussama_gtacj@live.fr>
2024-01-01 15:20:49 +0100

    feat: listing component, #8

---
+++ src/lib/view/clip-item.svelte
@@ -0,0 +1,186 @@
+<!-- TODO: Copy status when clicked -->
+
+<script lang="ts">
+	import MdCheck from 'svelte-icons/md/MdCheck.svelte';
+	import MdVisibility from 'svelte-icons/md/MdVisibility.svelte';
+	import MdContentCopy from 'svelte-icons/md/MdContentCopy.svelte';
+	import MdVisibilityOff from 'svelte-icons/md/MdVisibilityOff.svelte';
+	import type { TClip } from '../../core/types/clip.type';
+
+	export let clip: TClip;
+	let visible = false;
+	let copied = false;
+
+	$: content = clip.sensitive && !visible ? hideContent(clip.content) : clip.content;
+
+	const hideContent = (content: string | Blob) => {
+		if (typeof content === 'string') {
+			return content
+				.split('')
+				.map((e) => '•')
+				.join('');
+		}
+
+		return content;
+	};
+
+	const onCopy = (e: MouseEvent) => {
+		e.stopPropagation();
+		if (!visible && clip.sensitive) {
+			if (prompt('Password') !== '123') {
+				return;
+			}
+		}
+
+		copy();
+	};
+
+	const onToggleVisibility = (e: MouseEvent) => {
+		e.stopPropagation();
+		if (!visible && clip.sensitive) {
+			if (prompt('Password') !== '123') {
+				return;
+			}
+		}
+
+		visible = !visible;
+	};
+
+	const copy = () => {
+		if (typeof clip.content === 'string') {
+			navigator.clipboard.writeText(clip.content);
+		} else {
+			const clipboard = [new ClipboardItem({ [clip.content.type]: clip.content })];
+			navigator.clipboard.write(clipboard);
+		}
+
+		copied = true;
+		const timer = setTimeout(() => {
+			copied = false;
+			clearTimeout(timer);
+		}, 1000);
+	};
+</script>
+
+<div class="clip">
+	<button class="clip__content" on:click={onCopy}>
+		<div class="clip__info">
+			<h4 class="clip__title">{clip.title}</h4>
+			<p class="clip__content">{content}</p>
+		</div>
+
+		<div class="clip__controls">
+			{#if clip.sensitive}
+				<button class="clip__control clip__control--visibility" on:click={onToggleVisibility}>
+					{#if visible}
+						<MdVisibilityOff />
+					{:else}
+						<MdVisibility />
+					{/if}
+				</button>
+			{/if}
+
+			<button
+				class="clip__control clip__control--copy"
+				class:clip__control--copied={copied}
+				on:click={onCopy}
+			>
+				{#if copied}
+					<MdCheck />
+				{:else}
+					<MdContentCopy />
+				{/if}
+			</button>
+		</div>
+	</button>
+</div>
+
+<style lang="scss">
+	.clip {
+		$root: &;
+
+		&__content {
+			all: unset;
+
+			width: 100%;
+			height: 100%;
+			box-sizing: border-box;
+			padding: 2px 10px;
+
+			display: flex;
+			cursor: pointer;
+
+			transition-duration: 0.2s;
+			transition-property: background-color;
+
+			#{$root}__info {
+				flex: 1;
+
+				#{$root}__title {
+					padding: 0;
+
+					font-size: 12px;
+					text-transform: capitalize;
+					font-weight: var(--font-weight-bold);
+				}
+
+				#{$root}__content {
+					padding: 0;
+					display: inline;
+
+					font-size: 14px;
+					font-weight: var(--font-weight-light);
+				}
+			}
+
+			#{$root}__controls {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+
+				opacity: 0;
+				margin-left: auto;
+
+				transition-duration: 0.2s;
+				transition-property: opacity;
+
+				#{$root}__control {
+					all: unset;
+
+					width: 18px;
+					height: 18px;
+
+					opacity: 0.6;
+					color: rgb(184, 184, 184);
+
+					transition-duration: 0.2s;
+					transition-property: opacity;
+
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					&--copied {
+						color: #22c01c;
+					}
+
+					&:hover {
+						opacity: 1;
+					}
+
+					&:not(:last-of-type) {
+						margin-right: 10px;
+					}
+				}
+			}
+
+			&:hover {
+				background-color: rgb(250, 250, 250);
+
+				#{$root}__controls {
+					opacity: 1;
+				}
+			}
+		}
+	}
+</style>
---
+++ src/lib/view/clip-list.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { TClip } from '../../core/types/clip.type';
+	import ClipItem from './clip-item.svelte';
+
+	const clips: Array<TClip> = [
+		{
+			id: 'a',
+			content: 'lorem',
+			title: 'pri clip',
+			sensitive: false,
+			tags: []
+		},
+		{
+			id: 'f',
+			content: 'd399vv@ff',
+			title: 'fb pass',
+			sensitive: true,
+			tags: [{ name: 'acc' }]
+		},
+		{
+			id: 't',
+			content: '03169679',
+			title: 'acc id',
+			sensitive: false,
+			tags: [{ name: 'iam' }]
+		}
+	];
+</script>
+
+<div class="clips">
+	<ul class="clips-list">
+		{#each clips as clip}
+			<li class="clips-item">
+				<ClipItem {clip} />
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style lang="scss">
+	.clips {
+		$root: &;
+
+		&-list {
+			margin: 0;
+			padding: 0;
+			list-style-type: none;
+
+      border-radius: 5px;
+			border: 1px solid whitesmoke;
+
+			#{$root}-item:not(:last-of-type) {
+				border-bottom: 1px solid whitesmoke;
+			}
+		}
+	}
+</style>
