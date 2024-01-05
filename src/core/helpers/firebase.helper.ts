6fe2bb6a2bda935002e56856e55c0c086443d319
EOussama <oussama_gtacj@live.fr>
2024-01-05 14:12:42 +0100

    feat: Firebase integration, resolves #5

---
+++ src/core/helpers/firebase.helper.ts
@@ -0,0 +1,26 @@
+import * as config from '$env/static/public';
+import { initializeApp, type FirebaseApp } from 'firebase/app';
+
+
+
+export class FirebaseHelper {
+
+  private static app: FirebaseApp;
+
+  static getConfig() {
+    return {
+      appId: config.PUBLIC_FIREBASE_APP_ID,
+      apiKey: config.PUBLIC_FIREBASE_API_KEY,
+      projectId: config.PUBLIC_FIREBASE_PROJECT_ID,
+      authDomain: config.PUBLIC_FIREBASE_AUTH_DOMAIN,
+      measurementId: config.PUBLIC_FIREBASE_MEASUREMENT_ID,
+      storageBucket: config.PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: config.PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+    }
+  }
+
+  static init(): void {
+    const config = this.getConfig();
+    this.app = initializeApp(config);
+  }
+}
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,1 +1,6 @@
+import { FirebaseHelper } from '../core/helpers/firebase.helper';
+
+
+
+FirebaseHelper.init();
 export const prerender = true;
