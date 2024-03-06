83d9a229ebcae2bbaad013c626db51fc1258cf12
eoussama <oussama.essamadi@gmail.com>
2024-03-06 13:43:45 +0100

    feat: clip helper funcs, #51

--- src/lib/core/helpers/clip.helper.ts
+++ src/lib/core/helpers/clip.helper.ts
@@ -1,10 +1,13 @@
 import { v4 as uuid } from 'uuid';
 import { get } from 'svelte/store';
 
-import { appStore } from '../stores/app.store';
-import type { TClip } from '../types/clip.type';
-import { ClipType } from '../enums/clip-type.enum';
 import { UserHelper } from './user.helper';
+
+import { appStore } from '../stores/app.store';
+import { ClipType } from '../enums/clip-type.enum';
+
+import type { TClip } from '../types/clip.type';
+import type { TNullable } from '../types/nullable.type';
 import type { TUserData } from '../types/user-data.type';
 
 
@@ -17,12 +20,12 @@ export class ClipHelper {
 
   /**
    * @description
-   * Creates a new clip.
+   * Creates a new clip
    *
    * @param clip The clip to create
    */
   static create(clip: Partial<TClip>): Promise<TClip> {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       const newClip: TClip = {
         id: clip.id ?? uuid(),
         tags: clip.tags ?? [],
@@ -33,12 +36,45 @@ export class ClipHelper {
         creationTime: clip?.creationTime ?? Date.now()
       };
 
-      this.add(newClip);
-      resolve(newClip);
+      this.add(newClip).then(() => resolve(newClip)).catch(reject);
+    })
+  }
+  /**
+   * @description
+   * Updates an existing clip
+   *
+   * @param clip The clip to update
+   */
+  static update(clip: TClip): Promise<TClip> {
+    return new Promise(async (resolve, reject) => {
+      if (await this.exists(clip.id)) {
+        this.set(clip).then(() => resolve(clip)).catch(reject);
+      } else {
+        reject();
+      }
     })
   }
 
-  static add(clip: TClip): Promise<void> {
+  private static get(clipId: string): Promise<TNullable<TClip>> {
+    return new Promise((resolve, reject) => {
+      const userData = get(appStore).data;
+
+      if (userData) {
+        const clip = userData.clips.find(e => e.id === clipId);
+        resolve(clip ?? null);
+      } else {
+        reject();
+      }
+    });
+  }
+
+  private static exists(clipId: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.get(clipId).then(clip => resolve(Boolean(clip))).catch(reject);
+    });
+  }
+
+  private static add(clip: TClip): Promise<void> {
     return new Promise((resolve, reject) => {
       const userData = get(appStore).data;
 
@@ -53,6 +89,36 @@ export class ClipHelper {
           .set(userData?.id, updatedUserData)
           .then(() => resolve())
           .catch(() => reject());
+      } else {
+        reject();
+      }
+    });
+  }
+
+  private static set(clip: TClip): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const userData = get(appStore).data;
+
+      if (userData) {
+        const updatedUserData: TUserData = {
+          id: userData.id,
+          settings: userData.settings ?? {},
+          clips: [...(userData.clips ?? [])]
+        };
+
+        const clips = [...updatedUserData.clips];
+        const clipIndex = updatedUserData.clips.findIndex(e => e.id === clip.id);
+
+        updatedUserData.clips = [...clips.slice(0, clipIndex), clip, ...clips.slice(clipIndex + 1)]
+
+        if (clipIndex !== -1) {
+          UserHelper
+            .set(userData?.id, updatedUserData)
+            .then(() => resolve())
+            .catch(() => reject());
+        } else {
+          reject();
+        }
       } else {
         reject();
       }
