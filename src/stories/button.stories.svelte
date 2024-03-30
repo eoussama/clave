ead9ba9be7ddebedfffc49e6b55c7d0b7d9c3994
eoussama <oussama.essamadi@gmail.com>
2024-03-30 16:18:22 +0000

    button story, #58

---
+++ src/stories/button.stories.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+  import Button from '$lib/components/controls/button.svelte';
+
+  export const meta = {
+    title: 'Button',
+    component: Button
+  }
+</script>
+
+<script>
+  import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Button {...args} />
+</Template>
+
+<Story name="Default" args={{}} />
