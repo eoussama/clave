4debfc60ce5ed5a108379f53e8e5c0c24393b52f
eoussama <oussama.essamadi@gmail.com>
2024-08-23 15:28:54 +0100

    tags story, #58

---
+++ src/stories/tags.stories.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	import Tags from '$lib/components/controls/tags.svelte';
+
+	export const meta = {
+		title: 'Tags',
+		component: Tags
+	};
+</script>
+
+<script>
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Tags {...args} />
+</Template>
+
+<Story name="Default" args={{ label: 'Default' }} />
