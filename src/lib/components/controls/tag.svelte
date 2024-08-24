06a61cf79319b679b234709805da2c33f04ec27b
eoussama <oussama.essamadi@gmail.com>
2024-08-24 13:24:01 +0100

    tag interaction, #58

--- src/lib/components/controls/tag.svelte
+++ src/lib/components/controls/tag.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
-	import { v4 as uuid } from 'uuid';
+	import { createEventDispatcher } from 'svelte';
+	import MdRemove from 'svelte-icons/md/MdRemove.svelte';
 
 	import type { TTag } from '$lib/core/types/tag.type';
 
-	// TODO: default
 	// TODO: disabled
 	// TODO: readonly
+
+	/**
+	 * @description
+	 * Event dispatcher
+	 */
+	const dispatch = createEventDispatcher();
 
 	/**
 	 * @description
@@ -15,18 +19,80 @@
 	 */
 	export let tag: TTag;
 
-	onMount(() => {
-		if (!tag.id) {
-			tag.id = uuid();
+	/**
+	 * @description
+	 * If the tag is disabled
+	 */
+	export let disabled: boolean = false;
+
+	/**
+	 * @description
+	 * If the tag is readonly
+	 */
+	export let readonly: boolean = false;
+
+	/**
+	 * @description
+	 * Click handler
+	 */
+	const onClick = () => {
+		if (!disabled && !readonly) {
+			dispatch('remove', { tag });
 		}
-	});
+	};
 </script>
 
-<span class="tag">
-	{tag.text}
-</span>
+<button
+	class="tag"
+	class:tag--disabled={disabled}
+	class:tag--readonly={readonly}
+	on:click={onClick}
+>
+	<span class="tag__text">
+		{tag.text}
+	</span>
+
+	<span class="tag__remove">
+		{#if !readonly && !disabled}
+			<MdRemove />
+		{/if}
+	</span>
+</button>
 
 <style lang="scss">
 	.tag {
+		$root: &;
+
+		--tag-bg-color: transparent;
+		--tag-text-color: hsl(var(--color-primary-hsl), 70%);
+		--tag-border-color: hsl(var(--color-primary-hsl), 80%);
+
+		cursor: pointer;
+
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+
+		padding: 2px 6px;
+		border-radius: 10px;
+
+		color: var(--tag-text-color);
+		background-color: var(--tag-bg-color);
+		border: 1px solid var(--tag-border-color);
+
+		transition-duration: 0.2s;
+		transition-property: background-color;
+
+		&__text {
+			font-size: 10px;
+			font-weight: var(--font-weight-light);
+		}
+
+		&__remove {
+			width: 12px;
+			height: 12px;
+
+			margin-left: 4px;
+		}
 	}
 </style>
