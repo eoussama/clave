75c4d6cbb0b5bfa6d9ecf913edb209f566af6ab0
eoussama <oussama.essamadi@gmail.com>
2024-09-08 15:45:38 +0100

    feat: button colors, #74

---
+++ src/lib/core/enums/button-color.enum.ts
@@ -0,0 +1,42 @@
+/**
+ * @description
+ * Button colors
+ */
+export enum ButtonColor {
+
+  /**
+   * @description
+   * The default button color
+   */
+  Default,
+
+  /**
+   * @description
+   * The primary button color
+   */
+  Primary,
+
+  /**
+   * @description
+   * The secondary button color
+   */
+  Secondary,
+
+  /**
+   * @description
+   * The failure button color
+   */
+  Failure,
+
+  /**
+   * @description
+   * The warning button color
+   */
+  Warning,
+
+  /**
+   * @description
+   * The success button color
+   */
+  Success
+}
