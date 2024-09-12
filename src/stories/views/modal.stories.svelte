936c0449c4668e5bee84e2412b0a652784c23ba9
eoussama <oussama.essamadi@gmail.com>
2024-09-12 14:04:56 +0100

    feat: modal story, #74

---
+++ src/stories/views/modal.stories.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	import Modal from '$lib/components/views/modal.svelte';
+
+	export const meta = {
+		title: 'Views/Modal',
+		component: Modal,
+		tags: ['autodocs'],
+		argTypes: {}
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<Modal {...args} />
+</Template>
+
+<Story name="Default" />
