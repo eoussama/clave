76f993094ca9bab9d10f53959b341ff25bf0693d
eoussama <oussama.essamadi@gmail.com>
2024-09-12 14:03:16 +0100

    feat: modal component, #74

---
+++ src/lib/components/views/modal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div class="modal">
+	<div class="modal__element">
+		<div class="modal__head">
+			<h3 class="modal__title">Title</h3>
+
+			<div class="modal__controls">
+				<div class="modal__control modal__control--close"></div>
+			</div>
+		</div>
+
+		<div class="modal__body"></div>
+	</div>
+</div>
+
+<style lang="scss">
+	.modal {
+	}
+</style>
