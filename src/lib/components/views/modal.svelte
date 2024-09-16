273eb6fc9ef4a696821416865f86be704a58c54b
eoussama <oussama.essamadi@gmail.com>
2024-09-16 21:23:10 +0100

    feat: modal close, #74

--- src/lib/components/views/modal.svelte
+++ src/lib/components/views/modal.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-	// TODO: close
+	import { fade } from 'svelte/transition';
+
+	import Tip from '$lib/components/layout/tip.svelte';
+	import Button from '$lib/components/controls/button.svelte';
+
+	import MdClose from 'svelte-icons/md/MdClose.svelte';
+
+	import { TipPositiion } from '$lib/core/enums/tip-position.enum';
+	import { ButtonSize } from '$lib/core/enums/button-size.enum';
+	import { ButtonStyle } from '$lib/core/enums/button-style.enum';
+	import { ButtonColor } from '$lib/core/enums/button-color.enum';
+
 	// TODO: body
 	// TODO: state
 
@@ -8,6 +19,14 @@
 	 * The title of the modal
 	 */
 	export let title: string;
+
+	/**
+	 * @description
+	 * Closes the modal
+	 */
+	const onClose = () => {
+		console.log('TODO: close');
+	};
 </script>
 
 <div class="modal">
@@ -16,7 +35,18 @@
 			<h3 class="modal__title">{title}</h3>
 
 			<div class="modal__controls">
-				<div class="modal__control modal__control--close"></div>
+				<div class="modal__control modal__control--close" in:fade={{ duration: 200 }}>
+					<Tip message="Close" position={TipPositiion.Top}>
+						<Button
+							ripple
+							icon={MdClose}
+							size={ButtonSize.Small}
+							style={ButtonStyle.Fill}
+							color={ButtonColor.Primary}
+							on:click={onClose}
+						/>
+					</Tip>
+				</div>
 			</div>
 		</div>
 
