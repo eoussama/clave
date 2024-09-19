a4d0051dbe4c245194ee80ecd751a79deff34c6b
eoussama <oussama.essamadi@gmail.com>
2024-09-19 11:59:51 +0100

    feat: overlay component, #74

---
+++ src/lib/components/views/overlay.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="overlay">
+	<div class="overlay__background"></div>
+	<div class="overlay__content">Content</div>
+</div>
+
+<style lang="scss">
+	.overlay {
+		$root: &;
+
+		z-index: 10;
+		position: relative;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		width: 85%;
+		height: 100%;
+
+		margin: auto;
+		max-width: 350px;
+
+		&__background {
+			position: absolute;
+			left: 0;
+			top: 0;
+
+			width: 100%;
+			height: 100%;
+
+			background-color: rgba(var(--color-primary-rgb), 0.4);
+		}
+	}
+</style>
