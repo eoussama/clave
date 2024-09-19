8c58ce2b7f5bdd2d69c63a464f4979d3f321bcb3
eoussama <oussama.essamadi@gmail.com>
2024-09-19 11:59:56 +0100

    feat: overlay story, #74

---
+++ src/stories/views/overlay.stories.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+	import Overlay from '$lib/components/views/overlay.svelte';
+
+	export const meta = {
+		title: 'Views/Overlay',
+		component: Overlay,
+		tags: ['autodocs'],
+		argTypes: {}
+	};
+</script>
+
+<script lang="ts">
+	import { Story, Template } from '@storybook/addon-svelte-csf';
+</script>
+
+<Template let:args>
+	<div class="parent">
+		<Overlay {...args} />
+	</div>
+</Template>
+
+<Story name="Default" />
+
+<style lang="scss">
+	.parent {
+		width: 100%;
+		height: 250px;
+	}
+</style>
